df2581828fd674db92e20f4b56269243ec1893e3
martinerrazquin <martin.errazquin@hotmail.com>
2024-05-06 20:00:07 -0300

    Changed ListDictBuilder behavior for raise_mismatch=False, now accepts fill_value feature

--- tests/test_collections.py
+++ tests/test_collections.py
@@ -1,55 +1,107 @@
 import unittest
 from utils.collections import ListDictBuilder
 
+
 class TestListDictBuilder(unittest.TestCase):
     def test_build_complete_keys_passed(self):
-        c1 = [1,2,3,4]
-        c2 = ['a','b','c','d']
+        c1 = [1, 2, 3, 4]
+        c2 = ["a", "b", "c", "d"]
         c3 = [True, False, True, False]
-        keys = ['X',5,('a','b')]
-        
-        expected = {k:c for k,c in zip(keys,(c1,c2,c3))}
+        keys = ["X", 5, ("a", "b")]
+
+        expected = {k: c for k, c in zip(keys, (c1, c2, c3))}
 
         x = ListDictBuilder(keys)
-        for x1,x2,x3 in zip(c1,c2,c3):
-            item = {k:x for k,x in zip(keys, (x1,x2,x3))}
+        for x1, x2, x3 in zip(c1, c2, c3):
+            item = {k: x for k, x in zip(keys, (x1, x2, x3))}
             x.append(item)
         result = x.export()
         self.assertDictEqual(expected, result)
 
     def test_build_complete_keys_inferred(self):
-        c1 = [1,2,3,4]
-        c2 = ['a','b','c','d']
+        c1 = [1, 2, 3, 4]
+        c2 = ["a", "b", "c", "d"]
         c3 = [True, False, True, False]
-        keys = ['X',5,('a','b')]
-        
-        expected = {k:c for k,c in zip(keys,(c1,c2,c3))}
+        keys = ["X", 5, ("a", "b")]
+
+        expected = {k: c for k, c in zip(keys, (c1, c2, c3))}
 
         x = ListDictBuilder()
-        for x1,x2,x3 in zip(c1,c2,c3):
-            item = {k:x for k,x in zip(keys, (x1,x2,x3))}
+        for x1, x2, x3 in zip(c1, c2, c3):
+            item = {k: x for k, x in zip(keys, (x1, x2, x3))}
             x.append(item)
         result = x.export()
         self.assertDictEqual(expected, result)
 
     def test_mismatch_raises_when_enabled_extra(self):
-        x = ListDictBuilder({'X','Y'}, raise_mismatch=True)
+        x = ListDictBuilder({"X", "Y"}, raise_mismatch=True)
         with self.assertRaises(KeyError) as s:
-            x.append({'X':1,'Y':2,'Z':4})
-    
-    def test_mismatch_raises_when_enabled_missing(self):
-        x = ListDictBuilder({'X','Y'}, raise_mismatch=True)
-        with self.assertRaises(KeyError) as s:
-            x.append({'X':1})
+            x.append({"X": 1, "Y": 2, "Z": 4})
 
-    def test_mismatch_raises_when_disabled_missing(self):
-        x = ListDictBuilder({'X','Y'}, raise_mismatch=False)
+    def test_mismatch_raises_when_enabled_missing(self):
+        x = ListDictBuilder({"X", "Y"}, raise_mismatch=True)
         with self.assertRaises(KeyError) as s:
-            x.append({'X':1}) 
-    
+            x.append({"X": 1})
+
+    def test_mismatch_raises_when_disabled_missing_and_no_fill(self):
+        x = ListDictBuilder({"X", "Y"}, raise_mismatch=False)
+        with self.assertRaises(KeyError) as s:
+            x.append({"X": 1})
+
     def test_mismatch_no_raise_when_disabled_extra(self):
-        x = ListDictBuilder({'X','Y'}, raise_mismatch=False)
+        x = ListDictBuilder({"X", "Y"}, raise_mismatch=False)
         try:
-            x.append({'X':1,'Y':2,'Z':4})
+            x.append({"X": 1, "Y": 2, "Z": 4})
         except KeyError:
             self.fail("KeyError was wrongfully raised.")
+
+    def test_mismatch_no_raise_when_disabled_missing_with_fill(self):
+        x = ListDictBuilder({"X", "Y"}, raise_mismatch=False, fill_value=3)
+        try:
+            x.append({"X": 1})
+        except KeyError:
+            self.fail("KeyError was wrongfully raised.")
+
+    def test_raises_when_no_keys_but_fill_passed(self):
+        with self.assertRaises(ValueError) as s:
+            x = ListDictBuilder(raise_mismatch=False, fill_value=5)
+
+    def test_no_raise_when_keys_and_fill_passed(self):
+        x = ListDictBuilder({"X", "Y"}, raise_mismatch=False, fill_value=5)
+        try:
+            x.append({"X": 1})
+        except KeyError:
+            self.fail("KeyError was wrongfully raised.")
+
+    def test_build_complete_fill_value_scalar(self):
+        fill_v = 5
+        x = ListDictBuilder({"X", "Y"}, raise_mismatch=False, fill_value=fill_v)
+        x.append({})
+
+        expected = {"X": [fill_v], "Y": [fill_v]}
+
+        result = x.export()
+        self.assertDictEqual(expected, result)
+
+    def test_build_complete_fill_value_dict(self):
+        fill_v_x = 1
+        fill_v_y = 2
+        fill_dict = {"X": fill_v_x, "Y": fill_v_y}
+        x = ListDictBuilder({"X", "Y"}, raise_mismatch=False, fill_value=fill_dict)
+
+        x.append({"X": 5})
+        x.append({"Y": 6})
+
+        expected = {"X": [5, fill_v_x], "Y": [fill_v_y, 6]}
+
+        result = x.export()
+        self.assertDictEqual(expected, result)
+
+    def test_build_raises_fill_value_dict_missing_one_key(self):
+        fill_dict = {"X": 3}
+        x = ListDictBuilder({"X", "Y"}, raise_mismatch=False, fill_value=fill_dict)
+
+        x.append({"Y": 3})
+        
+        with self.assertRaises(KeyError):
+            x.append({"X": 3})
--- utils/collections.py
+++ utils/collections.py
@@ -13,40 +13,59 @@ class defaultdict_key(dict):
         self[key] = self.factory(key)
         return self[key]
 
+
 class ListDictBuilder:
     """
     Somewhat like a Pandas DataFrame but you build it by iteratively
     appending rows, which is not how you want to build a pandas df.
 
     keys (optional): list-like of keys. If None, will be inferred from first item appended.
-    raise_mismatch: whether to raise a KeyError when appending an item whose keys 
-    are not exactly the expected ones. Regardless of this value, all the required keys are 
-    always expected.
+    raise_mismatch: whether to raise a KeyError when appending an item whose keys
+    are not *exactly* the expected ones. If False, missing keys will be completed based
+    on the fill_values parameter, but exceding keys will nonetheless raise
+    fill_values: value with which to fill missing expected keys.  If it's a value, all missing
+    keys will be filled with said value. If it's a dict, the fill value will be looked up by key.
+    If a key is missing in both the fill_values dict and the item to be appended, will
+    raise KeyError. Non-None values *require* the keys parameter to be passed.
     """
-    def __init__(self, keys: Iterable[Hashable]|None =None, raise_mismatch: bool = True):
-        self.raise_mismatch = raise_mismatch
+
+    def __init__(
+        self,
+        keys: Iterable[Hashable] | None = None,
+        raise_mismatch: bool = True,
+        fill_value: dict[Hashable | Any] | Any | None = None,
+    ):
         if keys is not None:
             self.keys = set(keys)
             self.d = {k: [] for k in self.keys}
         else:
+            if fill_value is not None:
+                raise ValueError(
+                    "fill_value is not None but keys were not explicitly declared"
+                )
             self.keys = None
             self.d = None
+        self.raise_mismatch = raise_mismatch
+        match fill_value:
+            case None:
+                self.fill_value = {}
+            case dict():
+                self.fill_value = fill_value.copy()
+            case _:
+                self.fill_value = {k: fill_value for k in self.keys}
 
     def append(self, item: dict[Hashable, Any]):
         received_keys = set(item.keys())
         if self.keys == None:
             self.keys = received_keys
-            self.d = {k:[v] for k,v in item.items()}
+            self.d = {k: [v] for k, v in item.items()}
             return
         if received_keys != self.keys and self.raise_mismatch:
             raise KeyError(f"Expected keys {self.keys}, got {received_keys}")
-        if received_keys < self.keys:
-            raise KeyError(f"Missing keys {self.keys - received_keys}")
         for k in self.keys:
-            self.d[k].append(item[k])
+            if k not in item and k not in self.fill_value:
+                raise KeyError(f"Key {k} missing in item without a supplied fill value")
+            self.d[k].append(item.get(k, self.fill_value.get(k)))
 
     def export(self):
         return self.d
-            
-
-
